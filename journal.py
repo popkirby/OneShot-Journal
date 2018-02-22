025a776a0b33ad82da398d4faeffc7f3fcfc6468
popkirby <popkirby@gmail.com>
2018-02-22 23:12:51 +0900

    impletement i18n and do-your-thing for mac

--- journal.py
+++ journal.py
@@ -2,9 +2,9 @@ import os
 import sys
 import time
 
-from PyQt5.QtCore import Qt, QThread, pyqtSignal, QRect, QRectF, QTimer
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
-from PyQt5.QtGui import QIcon, QPixmap, QPainter
+from PyQt5.QtCore import Qt, QThread, pyqtSignal, QRect, QRectF, QTimer, QCoreApplication
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QSizePolicy
+from PyQt5.QtGui import QIcon, QPixmap, QPainter, QPalette
 
 pipe_path = '/tmp/oneshot-pipe'
 
@@ -39,14 +39,15 @@ class Journal(QWidget):
         self.setWindowFlags(self.windowFlags())
         self.setAttribute(Qt.WA_TranslucentBackground)
         self.setWindowTitle('OneShot Journal')
+        self.show()
         self.setMinimumSize(800, 600)
         self.setMaximumSize(800, 600)
         self.setGeometry(0, 0, 800, 600)
-        self.show()
     
     def change_image(self, name):
         self.pixmap = QPixmap(os.path.join(base_path, 'images', '{}.png'.format(name)))
         self.label.setPixmap(self.pixmap)
+
 
 class Niko(QWidget):
     def __init__(self, *args, **kwargs):
@@ -54,11 +55,60 @@ class Niko(QWidget):
         del kwargs['start_x'], kwargs['start_y']
         
         super().__init__(*args, **kwargs)
-        
-        self.frames = [QPixmap(os.path.join(base_path, 'images', 'niko{}.png'.format(n)) for n in range(1,4))]
 
-        self.setBackgroundRole(QPalette.Base)
-        self.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)        
+        self.label = QLabel(self)
+
+        self.frames = [QPixmap(os.path.join(base_path, 'images', 'niko{}.png'.format(n))) for n in range(1,4)]
+        self.drawNikoFrame(0)
+
+        self.setAttribute(Qt.WA_TranslucentBackground)
+
+        self.setWindowFlags( \
+                Qt.FramelessWindowHint | \
+                Qt.WindowStaysOnTopHint | \
+                Qt.NoDropShadowWindowHint)
+        self.show()
+
+        self.setMinimumSize(24 * 2, 32 * 2)
+        self.setMaximumSize(24 * 2, 32 * 2)
+        self.setGeometry(self.start_x, self.start_y, 24 * 2, 32 * 2)
+
+    def update(self, y):
+        offset = self.start_y - y
+        self.drawNikoFrame(offset)
+        self.move(self.start_x, y)
+
+    def drawNikoFrame(self, offset):
+        if offset % 32 >= 16:
+            frame = 1
+        elif (offset // 32) % 2 == 1:
+            frame = 0
+        else:
+            frame = 2
+
+        self.label.setPixmap(self.frames[frame])
+
+
+class NikoThread(QThread):
+    update = pyqtSignal(int)
+
+    def __init__(self, *args, **kwargs):
+        self.y, self.screen_y = kwargs['start_y'], kwargs['screen_y']
+        del kwargs['start_y'], kwargs['screen_y']
+        super().__init__(*args, **kwargs)
+
+
+    def run(self):
+        while True:
+            self.y += 2
+            if (self.y > self.screen_y):
+                break
+
+            self.update.emit(self.y)
+            time.sleep(1 / 60)
+
+        QCoreApplication.quit()
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
@@ -66,7 +116,11 @@ if __name__ == '__main__':
     if len(sys.argv) == 3:
         # "Niko-leaves-the-screen" mode
         x, y = int(sys.argv[1]), int(sys.argv[2])
-    
+        niko = Niko(start_x=x, start_y=y)
+
+        thread = NikoThread(start_y=y, screen_y=app.primaryScreen().size().height())
+        thread.update.connect(niko.update)
+        thread.start()
     else:
         # Author's Journal mode
         journal = Journal()
